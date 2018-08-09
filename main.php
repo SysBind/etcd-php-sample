fb65fbc15778494d8f929f2bfa84babc9440ec62
Asaf Ohayon <asaf@sysbind.co.il>
2018-08-10 00:30:08 +0300

    working php sammple (fixed autoload), ignoring composer.lock for now

--- main.php
+++ main.php
@@ -1,10 +1,23 @@
 <?php
 
+$loader = __DIR__ . '/vendor/autoload.php';
+
+if (!file_exists($loader)) {
+    die(
+        'You must set up the project dependencies, run the following commands:' . PHP_EOL .
+        'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL
+    );
+}
+
+require $loader;
+
+
 use LinkORB\Component\Etcd\Client;
 
 echo "hello etcd\n";
 
-$client = new Client("example-etcd-cluster");
+$client = new Client("example-etcd-cluster:2379");
 
 $client->set('/foo', 'fooValue');
 
